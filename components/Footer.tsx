e5f06b0226c5447dcb9cd1f620403860d04f5c54
splante1060-star <splante1060@gmail.com>
2026-08-23 12:58:46 -0400

    feat: file cleanup, stand-in header & footer added

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-50 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-600">
+        <p>
+          A simpler way to understand spending, plan ahead, and save for what
+          matters.
+        </p>
+        <p>Made by Sophie Plante ♡ </p>
+      </div>
+    </footer>
+  );
+}
