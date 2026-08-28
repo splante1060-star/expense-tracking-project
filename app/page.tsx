7abce6693ad7076dafddd1a2585263096819ca6d
splante1060-star <splante1060@gmail.com>
2026-08-28 17:41:54 -0400

    fix: refine landing experience for only not signed in users

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 import HeroSection from "@/components/landing-page/hero-section";
 import IntroSection from "@/components/landing-page/intro-section";
 
-export default function Home() {
+export default async function HomePage() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
-    <div>
+    <>
       <HeroSection />
       <IntroSection />
-    </div>
+    </>
   );
 }
